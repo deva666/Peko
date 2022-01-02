f63f01348b71a161780ae14a8270cbdad37fc5dd
Marko Devcic <madevcic@gmail.com>
2022-01-02 08:06:33 +0100

    launch instead of async

--- peko/src/test/java/com/markodevcic/peko/PekoServiceTest.kt
+++ peko/src/test/java/com/markodevcic/peko/PekoServiceTest.kt
@@ -1,8 +1,12 @@
 package com.markodevcic.peko
 
 import android.content.Context
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
@@ -29,12 +33,13 @@ class PekoServiceTest {
         val sut = PekoService(context, request, permissionRequesterFactory, dispatcher)
 
         runBlocking {
-            async {
+            launch {
                 delay(200)
                 channel.send(PermissionResult.Granted(listOf("BLUETOOTH")))
             }
             val result = sut.requestPermissions()
-            val grantedResult = result as? PermissionResult.Granted ?: throw IllegalStateException("result should be Granted")
+            val grantedResult = result as? PermissionResult.Granted
+                ?: throw IllegalStateException("result should be Granted")
             Assert.assertTrue(grantedResult.grantedPermissions.size == 1)
             Assert.assertTrue(grantedResult.grantedPermissions.contains("BLUETOOTH"))
         }
@@ -50,12 +55,13 @@ class PekoServiceTest {
         val sut = PekoService(context, request, permissionRequesterFactory, dispatcher)
 
         runBlocking {
-            async {
+            launch {
                 delay(200)
                 channel.send(PermissionResult.Granted(listOf("BLUETOOTH")))
             }
             val result = sut.requestPermissions()
-            val grantedResult = result as? PermissionResult.Granted ?: throw IllegalStateException("result should be Granted")
+            val grantedResult = result as? PermissionResult.Granted
+                ?: throw IllegalStateException("result should be Granted")
             Assert.assertTrue(grantedResult.grantedPermissions.size == 2)
             Assert.assertTrue(grantedResult.grantedPermissions.contains("BLUETOOTH"))
             Assert.assertTrue(grantedResult.grantedPermissions.contains("CAMERA"))
@@ -71,13 +77,14 @@ class PekoServiceTest {
         val sut = PekoService(context, request, permissionRequesterFactory, dispatcher)
 
         runBlocking {
-            async {
+            launch {
                 delay(200)
                 channel.send(PermissionResult.Denied.JustDenied(listOf("BLUETOOTH")))
             }
 
             val result = sut.requestPermissions()
-            val deniedResult = result as? PermissionResult.Denied ?: throw IllegalStateException("result should be Denied")
+            val deniedResult = result as? PermissionResult.Denied
+                ?: throw IllegalStateException("result should be Denied")
             Assert.assertTrue(deniedResult.deniedPermissions.contains("BLUETOOTH"))
         }
     }
@@ -91,7 +98,7 @@ class PekoServiceTest {
         val sut = PekoService(context, request, permissionRequesterFactory, dispatcher)
 
         runBlocking {
-            async {
+            launch {
                 delay(200)
                 channel.send(PermissionResult.Cancelled)
             }

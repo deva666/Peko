c55d79226ba7e04c0ad504f33716c2bfc399491b
Marko Devcic <madevcic@gmail.com>
2022-10-16 19:49:46 +0200

    format file

--- peko/src/main/java/com/markodevcic/peko/PermissionResult.kt
+++ peko/src/main/java/com/markodevcic/peko/PermissionResult.kt
@@ -1,7 +1,9 @@
 package com.markodevcic.peko
 
 sealed class PermissionResult {
+
 	data class Granted(val permission: String) : PermissionResult()
+
 	sealed class Denied(open val permission: String) : PermissionResult() {
 		data class NeedsRationale(override val permission: String) : Denied(permission)
 		data class DeniedPermanently(override val permission: String) : Denied(permission)
